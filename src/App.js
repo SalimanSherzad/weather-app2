3d773ef3aa63c3629e0a5b6d429529d46e593575
Saliman Sherzad <SalimanSherzad@gmail.com>
2022-09-22 20:35:21 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import './index.css';
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5?'
+const KEY = process.env.REACT_APP_API_KEY;
+
 
 function App() {
+  const [location, setLocation] = useState('');
+  const [weather, setWeather] = useState({});
+  const base = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e98bfd52e8a50215ab6427574ecd80d4`
+
+  const search = (event) => {
+    if (event.key === 'Enter') {
+      fetch(base)
+      .then(response => response.json())
+      .then(result => {
+        setWeather(result);
+        setLocation('');
+        console.log(result);
+      })
+    }
+  }
+
+  
+  const dateFunction = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={e => setLocation(e.target.value)}
+            value={location}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateFunction(new Date())}</div>
+          </div>
+          <div className="weatherBox">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°F
+            </div>
+            <div className="weather-des">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
